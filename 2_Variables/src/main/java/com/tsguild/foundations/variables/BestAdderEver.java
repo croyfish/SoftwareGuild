b1e7c44ba3544015e0f389a7a5221d9abb6d719e
croyfish <jeffcroyle36@yahoo.com>
2017-08-23 07:35:41 -0400

    JECRINDWRK-11 Best Adder Ever

---
+++ 2_Variables/src/main/java/com/tsguild/foundations/variables/BestAdderEver.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsguild.foundations.variables;
+
+/**
+ *
+ * @author jeffc
+ */
+public class BestAdderEver {
+    public static void main(String[] args) {
+        
+        int variable1, variable2, variable3, variableSum;
+        
+        variable1 = 12;
+        variable2 = -16;
+        variable3 = 127;
+        
+        System.out.println("Variable1 is " + variable1);
+        System.out.println("Variable2 is " + variable2);
+        System.out.println("Variable3 is " + variable3);
+        
+        variableSum = variable1 + variable2 + variable3;
+        
+        System.out.println("The sum is " + variableSum);
+        System.out.println("The sum is " + variableSum);
+        
+        
+    }
+}
