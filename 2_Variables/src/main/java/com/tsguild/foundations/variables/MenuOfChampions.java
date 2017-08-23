1e44109db1af6950fa79228dafe83c68bfa03f4b
croyfish <jeffcroyle36@yahoo.com>
2017-08-23 07:52:02 -0400

    JECRINDWRK-12 Menu Of Champions

--- 2_Variables/src/main/java/com/tsguild/foundations/variables/MenuOfChampions.java
+++ 2_Variables/src/main/java/com/tsguild/foundations/variables/MenuOfChampions.java
@@ -15,7 +15,23 @@ public class MenuOfChampions {
         String restaurantName, food1, food2, food3;
         float food1Price, food2Price, food3Price;
         
-        System.out.println()
+        restaurantName = "Mobile Munchies";
+        food1 = "Quesadillas";
+        food2 = "Enchiladas";
+        food3 = "Quesaladas";
+        food1Price = (float) 4.25;
+        food2Price = (float) 3.59;
+        food3Price = (float) 9.75;
+        
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("  ");
+        System.out.println("            Welcome to " + restaurantName + "!");
+        System.out.println("  ");
+        System.out.println(".oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.");
+        System.out.println("  ");
+        System.out.println("      " + food1 + "    $" + food1Price);
+        System.out.println("      " + food2 + "     $" + food2Price);
+        System.out.println("      " + food3 + "     $" + food3Price);
         
     }
  
