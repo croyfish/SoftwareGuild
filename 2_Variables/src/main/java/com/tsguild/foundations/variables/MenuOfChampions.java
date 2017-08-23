bf257f5b07f2bb165b4be6455916cdb675fa940c
croyfish <jeffcroyle36@yahoo.com>
2017-08-23 07:44:47 -0400

    JECRINDWRK-13 Album Page and Restaurant Site

---
+++ 2_Variables/src/main/java/com/tsguild/foundations/variables/MenuOfChampions.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsguild.foundations.variables;
+
+/**
+ *
+ * @author jeffc
+ */
+public class MenuOfChampions {
+    public static void main(String[] args) {
+        
+        String restaurantName, food1, food2, food3;
+        float food1Price, food2Price, food3Price;
+        
+        System.out.println()
+        
+    }
+ 
+}
