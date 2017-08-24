70e308e442d121ec6a6a88b37046b61a62e45786
croyfish <jeffcroyle36@yahoo.com>
2017-08-24 13:13:08 -0400

    JECRINDWRK-53 Different Kettle Of Fish

---
+++ 7_For/src/main/java/com/tsguild/foundations/flowcontrol/fors/DifferentKettleOfFish.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tsguild.foundations.flowcontrol.fors;
+
+/**
+ *
+ * @author jeffc
+ */
+public class DifferentKettleOfFish {
+    public static void main(String[] args) {
+        
+        for (int fish = 1; fish < 10; fish++){
+            if(fish == 3){
+                System.out.println("RED fish!");
+            }else if(fish == 4){
+                System.out.println("BLUE fish!");
+            } else{
+                System.out.println(fish + " fish!");
+            }
+
+        }
+                
+    }
+}
