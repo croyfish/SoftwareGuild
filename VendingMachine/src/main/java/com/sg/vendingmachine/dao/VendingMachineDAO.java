43cd7d142946b9ca099e80b3e90e08d9447608f4
croyfish <jeffcroyle36@yahoo.com>
2017-09-09 09:13:01 -0400

    JECRINDWRK-112 Vending Machine - DAO Interface

---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineDAO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.vendingmachine.dao;
+
+import com.sg.vendingmachine.dto.Item;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author jeffc
+ */
+public interface VendingMachineDAO {
+    
+    public List getAllItems();
+    
+    public Item getItem(String SKU);
+    
+    public BigDecimal getMoneyEntered();
+    
+    public void addMoney(BigDecimal money);
+    
+    public void writeInventory();
+    
+}
